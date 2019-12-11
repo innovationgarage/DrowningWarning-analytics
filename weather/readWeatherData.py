5bf0f8dad809f8397ffa895c95fe1203de81a384
asadisaghar <asadisaghar@gmail.com>
2019-12-11 13:20:27 +0100

    removed all usage of h5py

--- weather/readWeatherData.py
+++ weather/readWeatherData.py
@@ -1,4 +1,3 @@
-import h5py
 import numpy as np
 import pandas as pd
 import pyproj
@@ -7,31 +6,17 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import metpy.calc as mpcalc
 from metpy.units import units
-
-def readLocationForecast(filepath):
-    hf = h5py.File(filepath, 'r')
-    keys = list(hf.keys())
-    dataset = {}
-    i = 0
-    for i, data in enumerate(hf.values()):
-        dataset[keys[i]] = data
-    return dataset
     
-def dataset2df(dataset, use_keys, time_idx):
+def dataset2df(dataset, use_keys, time_step, time_idx):
     tdf = pd.DataFrame()
-    tdf['latitude'] = dataset['latitude'][()].flatten()
-    tdf['longitude'] = dataset['longitude'][()].flatten()
-    forecast_times = [datetime.datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') for ts in dataset['time'][()]]
-    tdf['forecast_time'] = forecast_times[time_idx]
+    tdf['latitude'] = dataset[uparam].latitude.values.flatten()
+    tdf['longitude'] = dataset[uparam].longitude.values.flatten()
+    tdf['forecast_time'] = time_step
     for col in use_keys:
-        tdf[col] = dataset[col][()][time_idx,0,:,:].flatten()
+        tdf[col] = dataset[uparam].values[time_idx,0,:,:].flatten()
     return tdf
 
-def calculateWindSpeedDirection(df, filepath, uparam, vparam, time_idx):
-    data = xr.open_dataset(filepath)
-    data = data.metpy.parse_cf([uparam, vparam])
-    data[uparam].metpy.convert_units('knots')
-    data[vparam].metpy.convert_units('knots')
+def calculateWindSpeedDirection(df, data, filepath, uparam, vparam, time_idx):
     wind_speed = mpcalc.wind_speed(data[uparam], data[vparam])
     wind_direction = mpcalc.wind_direction(data[uparam], data[vparam])
     df['wind_speed'] = wind_speed[time_idx,0,:,:].flatten()
@@ -42,13 +27,19 @@ camp = [69.961308, 18.703892]
 filepath = 'data/arome_arctic_full_2_5km_20191011T09Z.nc'
 fullshape = (949, 739)
 use_keys = ['x_wind_gust_10m', 'y_wind_gust_10m'] #"U-momentum of gusts in 10m height"m/s, "V-momentum of gusts in 10m height"m/s
-dataset = readLocationForecast(filepath)
+#dataset = readLocationForecast(filepath)
+uparam, vparam = use_keys
+dataset = xr.open_dataset(filepath)
+dataset = dataset.metpy.parse_cf([uparam, vparam])
+dataset[uparam].metpy.convert_units('knots')
+dataset[vparam].metpy.convert_units('knots')
 
-for time_idx in range(len(dataset['time'][()])):
-    print(dataset['time'][()][time_idx])
-    df = dataset2df(dataset, use_keys, time_idx)
-    df = calculateWindSpeedDirection(df, filepath, use_keys[0], use_keys[1], time_idx)
-    df.to_pickle('data/wind_{}.pkl'.format(dataset['time'][()][time_idx]))
+time_steps = dataset[uparam].time.values
+for time_idx, time_step in enumerate(time_steps):
+    print(time_step)
+    df = dataset2df(dataset, use_keys, time_step, time_idx)
+    df = calculateWindSpeedDirection(df, dataset, filepath, use_keys[0], use_keys[1], time_idx)
+    df.to_pickle('data/wind_{}.pkl'.format(time_step))
 
 # for time_idx in range(3):
 #     print(time_idx)
