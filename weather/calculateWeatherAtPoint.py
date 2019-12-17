77a243059d27b32dfb7016ea03da1c6a55857501
asadisaghar <asadisaghar@gmail.com>
2019-12-17 14:51:06 +0100

    nearest method added and tested for position and time-grid is too coarse

--- weather/calculateWeatherAtPoint.py
+++ weather/calculateWeatherAtPoint.py
@@ -27,7 +27,7 @@ def readWeatherData(filepath):
 
 def getRelevantRegion(data, goal_lon, goal_lat, goal_param):
     #convert lat,lon to x,y using the projection of the dataset
-    proj_wgs84 = pyproj.Proj(init="epsg:4326")
+    proj_wgs84 = pyproj.Proj("epsg:4326")
     data_crs = data[goal_param].metpy.cartopy_crs
     goal_x, goal_y = pyproj.transform(proj_wgs84, data_crs.proj4_params, goal_lon, goal_lat)
     grid_size = 2.5e3 #m
@@ -36,40 +36,49 @@ def getRelevantRegion(data, goal_lon, goal_lat, goal_param):
     plus_x = goal_x + n*grid_size    
     min_x = min(minus_x, plus_x)
     max_x = max(minus_x, plus_x)
-    print('X range', min_x, goal_x, max_x)
-
     minus_y = goal_y - n*grid_size
     plus_y = goal_y + n*grid_size
     min_y = min(minus_y, plus_y)
     max_y = max(minus_y, plus_y)
-    print('Y range', min_y, goal_y, max_y)
-    
     #select the region around the point at the right time
     region_array = data[goal_param].where((data.x>=min_x)&(data.x<=max_x)&(data.y>=min_y)&(data.y<=max_y))
     return region_array
+
+def getNearestForecast(data, goal_lon, goal_lat, goal_time, goal_param):
+    #convert lat,lon to x,y using the projection of the dataset
+    proj_wgs84 = pyproj.Proj("epsg:4326")
+    data_crs = data[goal_param].metpy.cartopy_crs
+    goal_x, goal_y = pyproj.transform(proj_wgs84, data_crs.proj4_params, goal_lon, goal_lat)
+    #select the closest grid
+    weather_array = data[goal_param].sel(x=goal_x, y=goal_y, time=goal_time, method='nearest')
+    return weather_array.values[0]
 
 #def main():
 boat_filepath = '../data/merged/capture_246058.txt'
 weather_filepath = 'data/arome_arctic_full_2_5km_20191011T09Z.nc'
 camp_location = [69.961308, 18.703892]
-point_lat = camp_location[0]
-point_lon = camp_location[0]
-point_time = '2019-10-11 11:16:40+00:00'
+goal_lat = camp_location[0]
+goal_lon = camp_location[0]
+goal_time = '2019-10-11 11:16:40+00:00'
 
 boat = pd.read_csv(boat_filepath)
 weather, lon, lat = readWeatherData(weather_filepath)
 
 uparam = 'wind_speed'
 vparam = 'wind_direction'
-uparam_data = getRelevantRegion(weather, point_lon, point_lat, uparam)
-vparam_data = getRelevantRegion(weather, point_lon, point_lat, vparam)
-ud = uparam_data.to_dataframe()
-ud.reset_index(level=[0,1,2,3], inplace=True)
-vd = vparam_data.to_dataframe()
-vd.reset_index(level=[0,1,2,3], inplace=True)
 
-wind = ud
-wind[vparam] = vd[vparam]
-wind.dropna(inplace=True)
+boat['wind_speed'] = boat.apply(lambda row: getNearestForecast(weather, row['long'], row['lat'], row['timestamp'], uparam), axis=1)
+boat['wind_direction'] = boat.apply(lambda row: getNearestForecast(weather, row['long'], row['lat'], row['timestamp'], vparam), axis=1)
+
+# uparam_data = getRelevantRegion(weather, goal_lon, goal_lat, uparam)
+# vparam_data = getRelevantRegion(weather, goal_lon, goal_lat, vparam)
+# ud = uparam_data.to_dataframe()
+# ud.reset_index(level=[0,1,2,3], inplace=True)
+# vd = vparam_data.to_dataframe()
+# vd.reset_index(level=[0,1,2,3], inplace=True)
+
+# wind = ud
+# wind[vparam] = vd[vparam]
+# wind.dropna(inplace=True)
 
 #main()
