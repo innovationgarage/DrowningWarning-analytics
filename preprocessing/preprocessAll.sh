20aa714d0eeaca42fb327c3f4b2ce0b83fa35018
Egil <egil.moller@freecode.no>
2019-12-27 15:43:06 +0100

    More bugfixes

--- preprocessing/preprocessAll.sh
+++ preprocessing/preprocessAll.sh
@@ -5,7 +5,7 @@ for f in $FILES
 do
     echo "Processing "$f" file..."
     bname=$(basename -- "$f")
-    python preprocessing/preprocess.py --ti data/raw/first/telespor.csv --to data/clean/$bname --ci $f --co data/clean/$bname --allout data/merged/$bname --starttime "2019-10-11 10:17:00"
+    python preprocessing/preprocess.py --ti data/raw/first/telespor.csv --to data/clean/telespor.$bname --ci $f --co data/clean/capture.$bname --allout data/merged/$bname --starttime "2019-10-11 10:17:00"
 done
 #cp data/first/capture_246058.txt.merged.csv all_data_1.csv
 
