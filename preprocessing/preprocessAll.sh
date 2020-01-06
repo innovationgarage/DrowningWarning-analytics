882da235b832943a6c243594f379b56a244310ae
Egil <egil.moller@freecode.no>
2020-01-06 13:59:47 +0100

    Bugfix for file naming. Now call track start time files raw/*/TRACKNAME.starttime

--- preprocessing/preprocessAll.sh
+++ preprocessing/preprocessAll.sh
@@ -6,15 +6,20 @@ mkdir -p data/merged
 for f in $FILES
 do
     echo "Processing "$f" file..."
-    bname="$(basename -- "$f")"
+    filename="$(basename -- "$f")"
+    basename="$(basename -- "$f" .txt)"
     dirname="$(dirname "$f")"
-    starttime="$(cat "$dirname/starttime")"
-    echo "  Start time: $starttime"
-    python preprocessing/preprocess.py \
-           --ti "$dirname/telespor.csv" \
-           --to data/clean/telespor.$bname \
-           --ci $f \
-           --co data/clean/capture.$bname \
-           --allout data/merged/$bname \
-           --starttime "$starttime"
+    if ! [ -e "$dirname/$basename.starttime" ]; then
+        echo "  Missing start time"
+    else
+        starttime="$(cat "$dirname/$basename.starttime")"
+        echo "  Start time: $starttime"
+        python preprocessing/preprocess.py \
+               --ti "$dirname/telespor.csv" \
+               --to data/clean/telespor.$filename \
+               --ci $f \
+               --co data/clean/capture.$filename \
+               --allout data/merged/$filename \
+               --starttime "$starttime"
+    fi
 done
